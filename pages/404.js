09702c659f6530afa3ac20c336ddbefcd465a8f6
richardingwe <richardingwe5@gmail.com>
2021-05-12 18:34:15 +0100

    added 404 page

---
+++ pages/404.js
@@ -0,0 +1,17 @@
+import styles from '../styles/404.module.css';
+import Link from 'next/link';
+import Layout from '../components/Layout';
+
+const NotFoundPage = () => {
+    return (
+        <Layout>
+            <div className={styles.error}>
+                <h1>404</h1>
+                <h4>Sorry, Page Not Found</h4>
+                <Link href='/'>Go Back Home</Link>
+            </div>
+        </Layout>
+    );
+};
+
+export default NotFoundPage;
