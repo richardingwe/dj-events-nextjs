a557fdfb761e13dcf0bab49ddec00ff4b73e48fd
richardingwe <richardingwe5@gmail.com>
2021-05-12 15:00:37 +0100

    .

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import styles from '../styles/Layout.module.css';
 
 const Layout = ({ title, keywords, description, children }) => {
     return (
@@ -8,6 +9,10 @@ const Layout = ({ title, keywords, description, children }) => {
                 <meta name='description' content={description} />
                 <meta name='keywords' content={keywords} />
             </Head>
+
+            <div className={styles.container}>
+                {children}
+            </div>
         </div>
     );
 };
