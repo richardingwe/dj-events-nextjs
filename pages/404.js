7ae71bca4e7c78f82d9190f6ae5f6ac020b92906
richardingwe <richardingwe5@gmail.com>
2021-05-12 18:39:35 +0100

    added the exclamation triangle icon to the index page

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,13 @@
 import styles from '../styles/404.module.css';
 import Link from 'next/link';
 import Layout from '../components/Layout';
+import { FaExclamationTriangle } from 'react-icons/fa';
 
 const NotFoundPage = () => {
     return (
         <Layout>
             <div className={styles.error}>
-                <h1>404</h1>
+                <h1><FaExclamationTriangle /> 404</h1>
                 <h4>Sorry, Page Not Found</h4>
                 <Link href='/'>Go Back Home</Link>
             </div>
