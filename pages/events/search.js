66a4813daf41821e415aca4d784aa5f5c06bd4ad
richardingwe <richardingwe5@gmail.com>
2021-05-15 08:10:35 +0100

    added the search page

---
+++ pages/events/search.js
@@ -0,0 +1,43 @@
+import EventItem from '@/components/EventItem';
+import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
+import qs from 'qs';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+const SearchPage = ({ events }) => {
+    const router = useRouter();
+    return (
+        <Layout title='Search Results'>
+            <Link href='/'>Go Back</Link>
+            <h1>Seach Results For: {router.query.term} </h1>
+            {events.length === 0 && <h3>No events to show</h3>}
+
+            {events.map((evt => (
+                <EventItem key={evt.id} evt={evt} />
+            )))}
+        </Layout>
+    );
+};
+export default SearchPage;
+
+export async function getStaticProps({ query: { term } }) {
+
+    const query = qs.stringify({
+        _where: {
+            _or: [
+                { name_contains: term },
+                { performers_contains: term },
+                { description_contains: term },
+                { venue_contains: term },
+            ]
+        }
+    });
+    const res = await fetch(`${API_URL}/events?${query}`);
+    const events = await res.json();
+
+    return {
+        props: { events },
+        revalidate: 1,
+    };
+}
