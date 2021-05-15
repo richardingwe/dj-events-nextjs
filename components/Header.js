381c516ba89502ddfa8189c3a9e9a7cc8210bde5
richardingwe <richardingwe5@gmail.com>
2021-05-15 21:48:21 +0100

    created the AuthContext

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,14 @@
 import Link from 'next/link';
+import { useContext } from 'react';
 import styles from '@/styles/Header.module.css';
 import Search from './Search';
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import AuthContext from '@/context/AuthContext';
 
 const Header = () => {
+
+    const { user } = useContext(AuthContext);
+
     return (
         <header className={styles.header}>
             <div className={styles.logo}>
@@ -21,18 +26,40 @@ const Header = () => {
                             Events
                         </Link>
                     </li>
-                    <li>
-                        <Link href='/events/add'>
-                            Add Event
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href='/account/login'>
-                            <a className="btn-secondary btn-con">
-                                <FaSignInAlt /> Login
-                            </a>
-                        </Link>
-                    </li>
+                    {user ? (
+                        // If logged in
+                        <>
+                            <li>
+                                <Link href='/events/add'>
+                                    <a>Add Event</a>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href='/account/dashboard'>
+                                    <a>Dashboard</a>
+                                </Link>
+                            </li>
+                            <li>
+                                <button
+                                    onClick={() => logout()}
+                                    className='btn-secondary btn-icon'
+                                >
+                                    <FaSignOutAlt /> Logout
+                            </button>
+                            </li>
+                        </>
+                    ) : (
+                        // If logged out
+                        <>
+                            <li>
+                                <Link href='/account/login'>
+                                    <a className='btn-secondary btn-icon'>
+                                        <FaSignInAlt /> Login
+                                    </a>
+                                </Link>
+                            </li>
+                        </>
+                    )}
                 </ul>
             </nav>
         </header>
