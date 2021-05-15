a039b3a314c4f60cbd3032c9af8ec1d69e03f73a
richardingwe <richardingwe5@gmail.com>
2021-05-15 09:51:36 +0100

    created the add event page

--- components/EventItem.js
+++ components/EventItem.js
@@ -6,7 +6,7 @@ const EventItem = ({ evt }) => {
     return (
         <div className={styles.event}>
             <div className={styles.img}>
-                <Image src={evt.image ? evt.image.formats.thumbnail.url : '.images/event-default.png'} width={170} height={100} />
+                <Image src={evt.image ? evt.image.formats.thumbnail.url : '/images/event-default.png'} width={170} height={100} />
             </div>
             <div className={styles.info}>
                 <span>
