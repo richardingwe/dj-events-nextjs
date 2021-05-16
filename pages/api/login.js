97a9561024bcd1ebeb18304570ff4937b0cf7c78
richardingwe <richardingwe5@gmail.com>
2021-05-16 10:43:51 +0100

    added cookie

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,5 +1,5 @@
 import { API_URL } from '@/config/index';
-
+import cookie from 'cookie';
 
 export default async (req, res) => {
     if (req.method === 'POST') {
@@ -19,6 +19,16 @@ export default async (req, res) => {
         const data = await strapiRes.json();
 
         if (strapiRes.ok) {
+            // set cookie
+            res.setHeader('Set-Cookie', cookie.serialize(
+                'Token', data.jwt, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV !== 'development',
+                maxAge: 60 * 60 * 24 * 7, // 1 week
+                sameSite: 'strict',
+                path: '/'
+            }
+            ));
 
             res.status(200).json({ user: data.user });
         } else {
